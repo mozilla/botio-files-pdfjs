9610bca63b67eaa1dfe9b60aeb523953036784b2
Artur Adib <arturadib@gmail.com>
2012-03-29 13:10:34 -0300

    nit

--- __on_push.js
+++ __on_push.js
@@ -5,6 +5,7 @@ require('shelljs/global');
 // Publish viewer to gh-pages
 //
 exec('node make web'); 
+
 // This dir should have its own .git/
 cd('build/gh-pages'); 
 exec('git commit -am "Auto-updating via bot"');
